7850a72090c20cc5f0efa3c91bd81e7f17187dc8
YC <lychgxnn@outlook.com>
2023-06-04 13:03:10 +0100

    Correct current year

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import datetime
+
+current_date = datetime.datetime.now()
+
+current_year = current_date.year
+
 print("Recipient's name")
 r_name = input()
 print("Year of birth")
@@ -7,8 +13,8 @@ msg = input()
 print("your name")
 y_name = input()
 
-age = 2023 - yob
+age = current_year - yob
 
-card = f"{r_name}，let's celebrate your {age} years of awesomeness!\nishing you a day filled with joy and laughter as you turn {age}\n\n{msg}\n\nWith love and best wishes,\n{y_name}"
+card = f"\n{r_name}，let's celebrate your {age} years of awesomeness!\nWishing you a day filled with joy and laughter as you turn {age}\n\n{msg}\n\nWith love and best wishes,\n{y_name}"
 
 print(card)
